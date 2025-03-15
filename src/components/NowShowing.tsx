2716b91876db9c99ea4e27f7607f8b6648427e9b
Carsten <clu@rts.dk>
2025-03-15 08:29:37 +0100

    corrected a-tag to Link

--- src/components/NowShowing.tsx
+++ src/components/NowShowing.tsx
@@ -1,7 +1,8 @@
 import { Movies } from "@/lib/types"
 import Image from "next/image"
+import Link from "next/link"
 export async function getData() {
-    let response =  await fetch("https://api.themoviedb.org/3/movie/now_playing", {
+    const response = await fetch("https://api.themoviedb.org/3/movie/now_playing", {
       headers: {
         accept: 'application/json',
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
@@ -15,6 +16,7 @@ export async function getData() {
   export default async function NowShowing() {
 
     const movies: Movies = await getData()
+    
 
     return (
         <>
@@ -22,8 +24,10 @@ export async function getData() {
             <article key={movie.id} className="w-[8.75rem] shrink-0 first:ml-6 last:mr-6 relative">
                 <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width="500" height="800" alt=""></Image>
                 <p>{movie.title}</p>
+                <p>{movie.id}</p>
+
                 <p>{movie.vote_average.toFixed(1)} / 10 IMDb</p>
-                <a className="absolute inset-0" href={`/detail/${movie.id}`}></a>
+                <Link className="absolute inset-0" href={`/detail/${movie.id}`}></Link>
             </article>
         ))}
         </>
