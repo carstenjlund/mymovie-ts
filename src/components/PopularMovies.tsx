b3fd5560b68466e5a0b567b6e4cff8e868118d61
Carsten <clu@rts.dk>
2025-03-15 08:31:14 +0100

    cleaned up unused code block. changed response to const and added relative to article to make link work properly

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -2,15 +2,9 @@ import {  type Movies } from "@/lib/types"
 import Image from "next/image"
 import GenrePills from "./GenrePills"
 
-const urls = ["https://api.themoviedb.org/3/movie/popular", "https://api.themoviedb.org/3/genre/movie/list?language=en"]
-const options = {
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
-    }
-}
+
 export async function getData() {
-  let response =  await fetch("https://api.themoviedb.org/3/movie/popular", {
+  const response =  await fetch("https://api.themoviedb.org/3/movie/popular", {
     headers: {
       accept: 'application/json',
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
@@ -30,7 +24,7 @@ if(!response.ok) throw new Error(`Failed to load data: ${response.statusText}`)
         <>
         
         {movies.results.map(movie =>(
-            <article key={movie.id} className="grid grid-cols-[8.75rem_1fr] gap-4 mb-6">
+            <article key={movie.id} className="grid grid-cols-[8.75rem_1fr] gap-4 mb-6 relative">
                 <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width="500" height="800" alt=""></Image>
                 <div>
                   <p>{movie.title}</p>
