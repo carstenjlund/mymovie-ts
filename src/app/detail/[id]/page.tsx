e03e0426dd47c2451b9e60a7a187269883eb4117
Carsten <clu@rts.dk>
2025-03-15 08:32:18 +0100

    fixed showing of release date

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { Release, ReleaseDates, type Movie } from "@/lib/types"
+import Image from 'next/image'
+import { ReleaseCountry, ReleaseDate, ReleaseResults, type Movie } from "@/lib/types"
 async function getSingleMovie(id: string) {
     const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?append_to_response=release_dates`, {
         headers: {
@@ -11,37 +12,38 @@ async function getSingleMovie(id: string) {
 }
 
 export default async function Page({ params }: { params: Promise<{ id: string }>}) {
+    
     const { id } = await params
     const movie: Movie  = await getSingleMovie(id)
-    console.log(movie);
 
-    function findoroRating(release_dates_array: ReleaseDates): string {
-        let us_dates = release_dates_array?.results?.find(element => element.iso_3166_1 == "US")
-        let rated: Release[] = us_dates?.release_dates.find(element => element.certification != "")
-        return rated.certification
+    function findRating(release_dates_array: ReleaseResults): string {
+        const us_dates = release_dates_array?.results?.find(element => element.iso_3166_1 === "US") as ReleaseCountry
+        const rated = us_dates?.release_dates.find(element => element.certification !== "") as ReleaseDate        
+        return rated?.certification || "N/A"
     }
     
-
     return (
         <>
-        <h1>movie detail {id}</h1>
+            <Image className="w-full" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} width="800" height="400" alt=""></Image>    
+            <h1>{movie.title}</h1>
+            <p>{movie.vote_average.toFixed(1)} / 10 IMDb</p>
 
-        <section>
-        <div>
-            <p>length:</p>
-        <p>{Math.floor(movie.runtime/60)}h {movie.runtime%60}m</p>
-        </div>
+            <section className="flex justify-between">
+            <div>
+                <p>length:</p>
+                <p>{Math.floor(movie.runtime/60)}h {movie.runtime%60}m</p>
+            </div>
+            <div>
+                <p>Language:</p>
+                <p>{movie.spoken_languages.map(language => (<span className="mr-1" key={language.english_name}>{language.english_name}</span> ))}</p>
+            </div>
+            <div>
+                <p>Rating:</p>
+                <p>{findRating(movie.release_dates)}</p>
+            </div>
 
-        <div>
-            <p>Language:</p>
-        <p>{movie.spoken_languages.map(language => (<span>{language.english_name}</span>))}</p>
-        </div>
-        <div>
-            <p>Language:</p>
-        <p>{findoroRating(movie.release_dates)}</p>
-        </div>
-
-        </section>
+            </section>
+            <p>{movie.overview}</p>
         </>
     )
 }
