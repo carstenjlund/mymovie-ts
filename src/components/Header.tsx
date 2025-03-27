f9320b12874b6165c755487da82feea6c54a0be6
Carsten <clu@rts.dk>
2025-03-27 12:21:44 +0100

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { MdOutlineDehaze } from "react-icons/md";
+import { Merriweather } from "next/font/google";
+
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
+  weight: "700",
+  subsets: ["latin"],
+});
+
+export default function Header() {
+    const pathname = usePathname();
+    
+
+    return (
+        <header className={`grid grid-cols-[3rem_1fr_3rem] items-center p-4 ${pathname.startsWith("/detail") ? "absolute top-0 left-0 w-full" : ""}`}>
+            {pathname === "/" ? <MdOutlineDehaze /> : <Link href="/"><IoMdArrowRoundBack /></Link>}
+          <h1 className={`${merriweather.className} text-center`}>{pathname === "/" ? "MyMovie" : ""}</h1>
+          <form className="text-right">
+            <input type="checkbox" name="darkmode" id="darmode" />
+          </form>
+        </header>
+    )
+
+}
