bf7ded13e1db9623907202fc9c2bd879a34e9fdb
Carsten <clu@rts.dk>
2025-03-30 14:03:11 +0200

    tweaked spacing and added no scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default async function Home() {
   return (
     <>
 
-    <section >
+    <section className="my-6">
     <header className="flex justify-between mb-6 px-6">
         <h2 className="font-bold text-2xl">Now Showing</h2>
        
         <button className="border px-4 py-1 rounded-full">see more</button>
       </header>
-        <div className="flex overflow-x-scroll gap-4">
+        <div className="flex overflow-x-scroll gap-4 no-scrollbar">
           <NowShowing />
         </div>
     </section>
